30b99d42da3a7bf76157e2c6ae8691df4b23e56a
anwar-opu <anwar9437@gmail.com>
2023-07-14 00:57:39 +0600

    calculate rectangle,circle,triangle

---
+++ AreaOfAnyShape.py
@@ -0,0 +1,14 @@
+width = int(input('Enter width for rectangle : '))
+heightForTriangle = int(input('Enter height for rectangle: '))
+areaOfRectangle = width * heightForTriangle
+print('Area of rectangle :', areaOfRectangle)
+
+radius = float(input('radius for circle'))
+pi = 3.14159
+areaOfCircle = pi * (radius ** 2)
+print('Area of Circle :', areaOfCircle)
+
+heightForTriangle = int(input('Enter height for triangle :'))
+base = int(input('Enter base for triangle :'))
+areaOfTriangle = 1 / 2 * heightForTriangle * base
+print('Area of triangle: ', areaOfTriangle)

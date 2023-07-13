e83e4e58697e610859fc0f8050f244c8cfe95833
anwar-opu <anwar9437@gmail.com>
2023-07-14 01:04:16 +0600

    basic numerical operation in python

---
+++ BasicNumericalOperation.py
@@ -0,0 +1,9 @@
+num1 = 5
+num2 = 3
+print('Sum :', num1 + num2)
+print('sub :', num1 - num2)
+print('multiple :', num1 * num2)
+print('division:', num1 / num2)
+print('Remainder :', num1 % num2)
+print('division (Floor) : ', num1 // num2)
+print('square : ', num1 ** num2)

136f799174d288a40dfe90d7f6d5a2706c4a3f9e
anwar-opu <anwar9437@gmail.com>
2023-07-14 12:03:38 +0600

    if else statement exercise

---
+++ studentResult.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter your result:"))
+
+if 70 <= marks < 90:
+    print("A+")
+elif 50 <= marks < 70:
+    print("A")
+elif 33 <= marks < 50:
+    print("A-")
+else:
+    print("fail")

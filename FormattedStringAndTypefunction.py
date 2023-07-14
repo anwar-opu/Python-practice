64e35a7be57738995dbf00146ee73f488b45654f
anwar-opu <anwar9437@gmail.com>
2023-07-14 11:57:20 +0600

    string formatting and type function

---
+++ FormattedStringAndTypefunction.py
@@ -0,0 +1,14 @@
+num1 = 20
+num2 = 30
+result = (num1 + num2)
+name = "anwar hossain"
+
+# type find out :
+print(type(result))
+print(type(name))
+
+# formatting :
+print(f"{num1} + {num2} = {num1 + num2}")
+
+print(name, end=" ")
+print("200110")

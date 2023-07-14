a3ce8d96cef0ef53b3a5b03e1e084f94ecdc1d2f
anwar-opu <anwar9437@gmail.com>
2023-07-14 11:54:20 +0600

    for loop statement

---
+++ ForLoopingStatement.py
@@ -0,0 +1,27 @@
+# for loop statements :
+# name = "anwar hossain"
+#
+# for i in name:
+#     print(i)
+#     if i == "n":
+#         print("this is a spacial charter")
+
+# colours = {"blue", "black", "red", "yellow"}
+#
+# for colour in colours:
+#     print(colour)
+#     for i in colour :
+#         print(i)
+# for k in range(5):  # range 4
+#     print(k)
+# print("\n")
+
+# for k in range(2, 6): # range 2 to 5
+#     print(k)
+
+# for k in range(5, 26, 5):
+#     print(k)
+num = int(input("enter any integer number :"))
+
+for i in range(1, 11):
+    print(num,"*", i, "= ", num * i)

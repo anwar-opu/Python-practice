3cd10acfb0ec3c7772b38adfefe714f95df171b8
anwar-opu <anwar9437@gmail.com>
2023-07-14 12:06:02 +0600

    different type of math related operation

---
+++ MathRelated.py
@@ -0,0 +1,10 @@
+from math import *
+
+print("max : ", max(20, 10))
+print("min : ", min(20, 10))
+print("power: ", pow(3, 2))
+print("squarer root :",sqrt(25))
+print("absolute :", abs(-20))
+print("round: ", round(2.5))
+print("floor :", floor(3.7))
+print("celling : ",ceil(3.7))

dc2b8155be986aac7c16bbb2f84c8ea6a3ab3288
anwar-opu <anwar9437@gmail.com>
2023-07-14 11:47:59 +0600

    type casting

---
+++ TypeCasting.py
@@ -0,0 +1,24 @@
+num1 = int(input('Enter first number:'))
+num2 = int(input('Enter second number:'))
+num3 = '3'
+
+string_num = int(num3)
+print(ord('a'))
+
+print(type(num3))
+
+print(num1, 'and', num2, ',', num3)
+result = num1 + num2 + string_num
+print('result: ', result)
+
+result = num1 - num2
+print('result: ', result)
+
+result = num1 * num2
+print('result : ', result)
+
+result = num1 / num2
+print('result : ', result)
+
+result = num1 % num2
+print('result : ', result)

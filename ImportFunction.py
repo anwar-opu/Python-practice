2f8307d23c4bbb35b3bb84f872f2a5940ad968dd
anwar-opu <anwar9437@gmail.com>
2023-08-07 15:37:30 +0600

    import  Function in Python

---
+++ ImportFunction.py
@@ -0,0 +1,16 @@
+from math import sqrt, floor as fl, pi
+
+from anwar import welcome, anwar, sum as s
+
+print(sqrt(9))
+print(fl(9.5))
+print(pi)
+welcome()
+print(anwar)
+
+a = 5
+b = 6
+print(s(a, b))
+
+# import math
+# print(dir(math))
---
+++ anwar.py
@@ -0,0 +1,9 @@
+def welcome():
+    print("hey welcome my friend")
+
+
+def sum(a, b):
+    return a + b
+
+
+anwar = "A good boy"
